b3a016fc39f34d6c0fa8788e91d16f1a93c7d69a
Sourabhaswalrealcoderz <86648698+Sourabhaswalrealcoderz@users.noreply.github.com>
2023-07-28 13:11:39 +0530

    Update UserController.java

--- src/main/java/com/realcoderz/fullstack/backend/controller/UserController.java
+++ src/main/java/com/realcoderz/fullstack/backend/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 // Author : Sourabh Aswal T-30may2023
 @RestController
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
