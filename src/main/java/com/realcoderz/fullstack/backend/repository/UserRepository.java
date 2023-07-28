445942c8f15b4bcfe98246cee3b5e52221d75419
Sourabhaswalrealcoderz <86648698+Sourabhaswalrealcoderz@users.noreply.github.com>
2023-07-28 13:23:52 +0530

    Update UserRepository.java

--- src/main/java/com/realcoderz/fullstack/backend/repository/UserRepository.java
+++ src/main/java/com/realcoderz/fullstack/backend/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.realcoderz.fullstack.backend.repository;
 
 import com.realcoderz.fullstack.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
