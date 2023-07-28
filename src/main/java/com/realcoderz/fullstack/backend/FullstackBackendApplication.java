a4d60a46062a1a203f44a0caae0587bddc208fb8
Sourabhaswalrealcoderz <86648698+Sourabhaswalrealcoderz@users.noreply.github.com>
2023-07-28 13:19:10 +0530

    Update FullstackBackendApplication.java

--- src/main/java/com/realcoderz/fullstack/backend/FullstackBackendApplication.java
+++ src/main/java/com/realcoderz/fullstack/backend/FullstackBackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.realcoderz.fullstack.backend.model")	
 public class FullstackBackendApplication {
 
 	public static void main(String[] args) {
